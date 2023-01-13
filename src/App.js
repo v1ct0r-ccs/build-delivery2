c8e9ef4a576b6c3c5444d24eb265d4eea98ad09c
Victor Bruno <v1ct0r.ccs29@gmail.com>
2023-01-13 12:59:50 +0100

    exercicio pages

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import './App.css';
+import Pricing from './componentes/Pricing/Pricing';
+
+function App() {
+  return (
+    <div className="App">
+      <Pricing />
+    </div>
+  );
+}
+
+export default App; 
